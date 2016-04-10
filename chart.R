3363a985fc91338252b161177b2386d619fb5ee1
preis <preis@55953d02-9106-0410-9835-bf18e61e9b54>
2016-04-10 19:39:23 +0000

    Add data-label for last item, new stats

--- chart.R
+++ chart.R
@@ -1,21 +1,25 @@
 library(parsedate)
 data <- read.csv("stats.csv", stringsAsFactors=FALSE, sep=",", quote="")
+labeldata <- data[nrow(data), ]
 
 library(reshape2)
 datam <- melt(data, id.vars="Date", measure.vars=
-	# when you change the order here also adjust it below for the legent
+	# when you change the order here also adjust it below for the legend
 	c("Open.overall","Actual.bugs","Workable.bugs","Needinfo","Enhancements","Bugs.with.patch"))
+labeldatam <- melt(labeldata, id.vars="Date", measure.vars=c("Open.overall","Actual.bugs","Enhancements",
+                                                   "Needinfo","Workable.bugs","Bugs.with.patch"))
 
 library(ggplot2)
 
-ggplot(datam, aes(x=parse_date(Date, approx = TRUE), y=value, colour=variable)) +
+ggplot(datam, aes(x=parse_date(Date, approx = TRUE), y=value, colour=variable, label=value)) +
 	# draw lines with dots and inner white-circles to get a "subway map"-like effect
     geom_line(size = 2) +
     geom_point(size = 3) +
     geom_point(size = 1.50, color = "white") +
     # add fitted regression lines
-    geom_smooth(data=subset(datam, parse_date(Date, approx = TRUE) >= parse_date("2015-08-10 10:21")), 
+    geom_smooth(data=subset(datam, parse_date(Date, approx = TRUE) >= parse_date("2015-08-10 10:21")),
                             method="lm", level=0.99, linetype="dashed") +
+    geom_text(data = labeldatam, hjust=1, vjust=0, angle = 90) +
     # pin the y-axis at zero
     expand_limits(y=0) +
     # start after the change in how we compute the values, it's actually at 2015-08-10 10:21
@@ -26,15 +30,15 @@ ggplot(datam, aes(x=parse_date(Date, approx = TRUE), y=value, colour=variable)) 
     # add more ticks on the y-axis
     scale_y_continuous(breaks=seq(0,1000,by=100)) +
     # set a title for the graph
-    ggtitle("Open bugs in Apache POI") + 
+    ggtitle("Open bugs in Apache POI") +
     # set the default black/white theme
     theme_bw() +
     # legend styling
     theme(legend.position="right",
           legend.key = element_rect(fill = "white")) +
     scale_colour_discrete(labels=
-		# this list needs to match the order used above!
-		c("Open","Bugs","Workable Bugs","Needinfo","Enhancements","Bugs with Patch")) +
+        # this list needs to match the order used above!
+        c("Open","Bugs","Workable Bugs","Needinfo","Enhancements","Bugs with Patch")) +
     guides(colour=guide_legend(title=NULL))
 
-ggsave("stats.svg", width=210, height=148, units="mm")
+ggsave("stats.svg", width=300, height=250, units="mm")
